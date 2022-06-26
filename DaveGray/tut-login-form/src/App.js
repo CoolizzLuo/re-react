c9b496d854dca0ea0e985376ff5ba81ccd72f3a8
Enzo Luo <duenzo1010@gmail.com>
2022-06-27 00:56:18 +0800

    feat: tut-login-form

---
+++ DaveGray/tut-login-form/src/App.js
@@ -0,0 +1,12 @@
+import Register from './Register';
+import Login from './Login';
+
+function App() {
+  return (
+    <main className='App'>
+      <Login />
+    </main>
+  );
+}
+
+export default App;
